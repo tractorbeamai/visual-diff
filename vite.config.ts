c35a0373815cc23009930b2330ba74ad7b61fedf
wadefletch <wbfletch@gmail.com>
2026-02-11 06:05:36 -0600

    2026-02-11T12:05:36

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     tanstackRouter({
       routesDirectory: "src/browser/routes",
